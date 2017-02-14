c2c21fdf209829fd53cf9e56ba4f180ed05a996d
Jake Campa and Zach Swanson <zach@>
2017-02-14 13:27:12 -0800

    create project directory and basic silex file structure

---
+++ app/app.php
@@ -0,0 +1,15 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Calendar.php";
+
+    $app = new Silex\Application();
+    $app['debug'] = true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+    ));
+
+
+    return $app;
+ ?>
